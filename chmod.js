2fcf277ac16415968a87ed2cec470ed75eadce50
yuyutti <yuyutti@yuyutti.com>
2026-01-30 04:21:54 +0900

    Linux環境にて都度chmodしなくていいように変更

---
+++ chmod.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+if (process.platform !== "linux") {
+    process.exit(0);
+}
+
+const binPath = path.join(
+    __dirname,
+    "..",
+    "CSproj",
+    "bin",
+    "Release",
+    "net8.0",
+    "linux-x64",
+    "publish",
+    "FortniteReplayAnalysis"
+);
+
+try {
+    fs.chmodSync(binPath, 0o755);
+    console.log("[fortnite-replay-analysis] chmod +x applied");
+} catch (e) {
+}
